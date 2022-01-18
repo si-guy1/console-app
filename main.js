72aadfb2dd283905a39616a4f136b6bbdf985614
si-guy1 <82059095+si-guy1@users.noreply.github.com>
2022-01-18 12:50:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name = "Silas";
+var last_name = "Goofball";
+console.log(first_name);
+console.log(last_name);
+var full_name = first_name + last_name;
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b;
+console.log(add);
